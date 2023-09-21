b85e1d60003137676ac63fdf93b3380b7087f248
Saad Niazi <saadniazifed+1@gmail.com>
2023-09-21 19:03:35 +0500

    RPS UI
    Finished the basic RPS UI.

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ let computerScore = 0;
 let tiedScore = 0;
 let totalScore = userScore + computerScore + tiedScore;
 
-playerSelection = prompt("Please make a choice!");
+const button = document.querySelectorAll("button");
+const winLoseDraw = document.querySelector("#win-lose-draw");
+const result = document.querySelector("#result");
+
+button.forEach((buttons) =>
+  buttons.addEventListener("click", (event) => {
+    buttonClicked(event);
+  })
+);
 
 const getComputerChoice = () => {
   const computerChoice = Math.floor(Math.random() * 3);
@@ -18,60 +26,57 @@ const getComputerChoice = () => {
   }
 };
 
-const computerSelection = getComputerChoice();
+const buttonClicked = (event) => {
+  const computerSelection = getComputerChoice();
+  const playerChoice = event.target.value;
+  playRound(playerChoice, computerSelection);
+};
 
 const playRound = (playerChoice, computerChoice) => {
   playerChoice.toLowerCase();
   if (playerChoice === "rock") {
     if (computerChoice === "rock") {
       tiedScore++;
-      return "Computer also chose rock. The game is tied!";
+      winLoseDraw.textContent = "Computer also chose rock. The game is tied!";
     } else if (computerChoice === "paper") {
       computerScore++;
-      return "Computer chose Paper. you lose!";
+      winLoseDraw.textContent = "Computer chose Paper. you lose!";
     } else if (computerChoice === "scissors") {
       userScore++;
-      return "Computer chose Scissors. You won!";
+      winLoseDraw.textContent = "Computer chose Scissors. You won!";
     }
   } else if (playerChoice === "paper") {
     if (computerChoice === "rock") {
       ++userScore;
-      return "Computer chose rock. You won!";
+      winLoseDraw.textContent = "Computer chose rock. You won!";
     } else if (computerChoice === "paper") {
       ++tiedScore;
-      return "Computer also chose Paper. Its a draw!";
+      winLoseDraw.textContent = "Computer also chose Paper. Its a draw!";
     } else if (computerChoice === "scissors") {
       ++computerScore;
-      return "Computer chose scissors. You lose!";
+      winLoseDraw.textContent = "Computer chose scissors. You lose!";
     }
   } else if (playerChoice === "scissors") {
     if (computerChoice === "rock") {
       ++computerScore;
-      return "Computer chose rock. You lose!";
+      winLoseDraw.textContent = "Computer chose rock. You lose!";
     } else if (computerChoice === "paper") {
       ++userScore;
-      return "Computer chose paper. You win!";
+      winLoseDraw.textContent = "Computer chose paper. You win!";
     } else if (computerChoice === "scissors") {
       ++tiedScore;
-      return "Computer also chose scissors. It's a draw!";
+      winLoseDraw.textContent = "Computer also chose scissors. It's a draw!";
     }
   }
 };
 
 const game = () => {
-  for (let i = 0; i < 5; i++) {
-    if (totalScore <= 5) {
-      playerSelection = prompt("Please make a choice!"); // open the blank prompt window
-      playRound(playerSelection, computerSelection);
-    }
-  }
-
   if (userScore > computerScore) {
-    console.log("User wins!");
+    result.textContent = "User wins!";
   } else if (computerScore > userScore) {
-    console.log("Computer Wins!");
-  } else {
-    console.log("It's a tie!");
+    result.textContent = "Computer Wins!";
+  } else if (tiedScore > userScore && computerScore) {
+    result.textContent = "It's a tie!";
   }
 };
 
